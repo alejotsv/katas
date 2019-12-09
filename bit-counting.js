c5ddeb0a37336ecf67f634b38348bc17f3b759d7
Alejandro Salas <asalas.wd@outlook.com>
2019-12-08 19:26:48 -0500

    complete bit count kata

--- bit-counting.js
+++ bit-counting.js
@@ -2,3 +2,18 @@
 
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
+function countBits(int) {
+  intBit = int.toString(2).toString();
+  let result = 0;
+  
+  for (let i=0; i<intBit.length; i++){
+    if(intBit[i] === '1'){
+      result += 1;
+    }
+  }
+  
+  console.log(intBit);
+  return result;
+}
+
+countBits(2141421);

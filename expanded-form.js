666554156d083cd4aebe83917f3f39402ced9e5f
Alejandro Salas <asalas.wd@outlook.com>
2019-12-09 07:32:35 -0500

    start expanded number kata

---
+++ expanded-form.js
@@ -0,0 +1,6 @@
+// Instructions: Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// Step 1: check length of number. The length determines how many numbers need to be added.
+// Step 3: add zeroes as many times as the variable value.
+// Step 2: set a variable as the length and decrease within the for loop.

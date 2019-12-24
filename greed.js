e9ab566a533653bd1bbd24450585bda57de4fdf2
Alejandro Salas <asalas.wd@outlook.com>
2019-12-23 21:47:40 -0500

    start greed kata

---
+++ greed.js
@@ -0,0 +1,3 @@
+Instructions:
+// Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+

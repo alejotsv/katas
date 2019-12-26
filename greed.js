da2abdc6fa1aeab95c4dcba46a6ac0aa2d9b3964
Alejandro Salas <asalas.wd@outlook.com>
2019-12-26 10:47:20 -0500

    evauluate each case and add score to result

--- greed.js
+++ greed.js
@@ -19,18 +19,78 @@
 // 2 4 4 5 4   400 + 50 = 450
 // In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
 
-
 function score(dice) {
 
+  let result = 0;
   let newArr = [];
   for (let i=1; i<=6; i++){
     newArr.push(dice.filter((num) => num === i))
   }
 
   console.log(newArr);
+
+  switch (newArr[0].length){
+    case 0:
+    break;
+
+    case 1:
+    result += 100;
+    break;
+
+    case 2:
+    result += 200;
+    break;
+
+    case 3:
+    result += 1000;
+    break;
+
+    case 4:
+    result += 1100;
+    break;
+
+    case 5:
+    result += 1200;
+    break;
+  }
+
+  switch (newArr[4].length){
+    case 0:
+    break;
+
+    case 1:
+    result += 50;
+    break;
+
+    case 2:
+    result += 100;
+    break;
+
+    case 4:
+    result += 50;
+    break;
+
+    case 5:
+    result += 100;
+    break;
+  }
+
+
+  let score = 600;
+  for (let i=5; i>0; i--){
+    if (newArr[i].length >= 3){
+      result += score;
+    }
+    score -= 100;
+  }
+
+
+return result;
+
+  
   
 }
 
-let arr = [1,3,4,5,1];
+let arr = [2,1,4,4,4];
 
 score(arr);

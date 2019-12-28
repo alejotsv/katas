1cef99906862cc3f65ff28999915d4125e0a1e4c
Alejandro Salas <asalas.wd@outlook.com>
2019-12-27 20:50:01 -0500

    complete snail kata

--- snail.js
+++ snail.js
@@ -9,11 +9,29 @@
 function snail(arr) {
   let result = [];
 
-  addFirstRow(arr,result);
-  addEachLast(arr,result);
-  addReverseLastRow(arr,result);
-  console.log(result);
-  console.log(arr);
+  if (arr.length === 1){
+    return arr[0];
+  }
+
+  if (arr[0].length){
+
+    while(arr.length > 0){
+      addFirstRow(arr,result);
+      if (arr.length === 1){
+        addReverseLastRow(arr,result);
+      }
+      if (arr.length > 1){
+        addEachLast(arr,result);
+        addReverseLastRow(arr,result);
+      }
+      if (arr.length > 1){
+        addEachFirst(arr,result);
+      }
+    }
+
+  }
+  
+  return result;
 
 }
 
@@ -28,7 +46,7 @@ function addEachLast(arr,result){
   let currentArr;
   for (let i=0; i<arr.length-1; i++){
     currentArr = arr[i];
-    result.push(currentArr[currentArr.length-1])    
+    result.push(...currentArr.splice(currentArr.length-1,1));    
   }
 }
 
@@ -38,18 +56,31 @@ function addReverseLastRow(arr,result) {
   arr.splice(arr.length-1,1);  
 }
 
+// Function to add to result the remaining first elements of each row and delete them from original array
 function addEachFirst(arr,result) {
-  
+  let currentArr;
+  for (let i=arr.length-1; i>0; i--){
+    currentArr = arr[i];
+    result.push(...currentArr.splice(0,1));
+  }
 }
   
   
 
 let array = [
-  [1,2,3,4],
-  [5,6,7,8],
-  [9,10,11,12],
-  [13,14,15,16]
+  [1, 2, 3, 4, 5, 6],
+  [20, 21, 22, 23, 24, 7],
+  [19, 32, 33, 34, 25, 8],
+  [18, 31, 36, 35, 26, 9],
+  [17, 30, 29, 28, 27, 10],
+  [16, 15, 14, 13, 12, 11]
+]
+
+let arrayS = [
+  [1,2],
+  [4,3]
 ]
 
 
 snail(array);
+

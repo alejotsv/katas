5e42118ea5b8fd9d4febfdedf3e5d79a62ca0e15
Alejandro Salas <asalas.wd@outlook.com>
2021-05-13 18:06:34 -0400

    create kata to show sum as string

---
+++ sum-string.js
@@ -0,0 +1,10 @@
+// Instructions:
+
+// Given the string representations of two integers,
+// return the string representation of the sum of those integers.
+
+function sumStrings(a,b) { 
+  let c = parseInt(a)+parseInt(b);
+  let result = c.toString();
+  return result
+}

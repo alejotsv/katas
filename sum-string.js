bd91dae4918de88306aaae8725fcc3c960705445
Alejandro Salas <asalas.wd@outlook.com>
2021-05-13 18:15:12 -0400

    update to account for BigInt

--- sum-string.js
+++ sum-string.js
@@ -3,8 +3,19 @@
 // Given the string representations of two integers,
 // return the string representation of the sum of those integers.
 
-function sumStrings(a,b) { 
-  let c = parseInt(a)+parseInt(b);
-  let result = c.toString();
+function sumStrings(a,b){
+  let c = BigInt(0);
+  let d = BigInt(0);
+
+  if(a.length>0){
+    c = BigInt(a);
+  }
+
+  if(b.length>0){
+    d = BigInt(b);
+  }
+
+  let sum = c + d;
+  let result = sum.toString();
   return result
 }

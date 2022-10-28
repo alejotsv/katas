f64c116d415d0a17ecd3ff0122d8394da497dcd5
Alejandro Salas <asalas.wd@outlook.com>
2022-10-27 22:53:59 -0400

    update findInRow function to go through each element of the array

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -1,30 +1,30 @@
 function sudoku(puzzle) {
-  console.log(puzzle);
-  //return the solved puzzle as a 2d array of 9 x 9 
-}
+  findInRow(4, puzzle[0]);
+};
 
 let puzzle = [
-  [5,3,0,0,7,0,0,0,0],
-  [6,0,0,1,9,5,0,0,0],
-  [0,9,8,0,0,0,0,6,0],
-  [8,0,0,0,6,0,0,0,3],
-  [4,0,0,8,0,3,0,0,1],
-  [7,0,0,0,2,0,0,0,6],
-  [0,6,0,0,0,0,2,8,0],
-  [0,0,0,4,1,9,0,0,5],
-  [0,0,0,0,8,0,0,7,9]];
+            [5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]];
 
-
-sudoku(puzzle);
+let possibilities = {};
 
 function findInRow(num, arr){
-
+  arr.forEach((e) => console.log(e));
 }
 
 function findInColumn(num, matrix){
-
+  
 }
 
 function findInSquare(num, matrix){
-
+  
 }
+
+sudoku(puzzle);

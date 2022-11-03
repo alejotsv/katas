acfde4a9be32a184067836dc3ae65b81fa79d14b
Alejandro Salas <asalas.wd@outlook.com>
2022-11-02 22:22:22 -0400

    Complete function to check if string is isogram

--- isogram.js
+++ isogram.js
@@ -1,8 +1,14 @@
 function isIsogram(str){
-  let arr = str.split("");
-  arr.forEach(e => console.log(e));
-  
-  
+  let isogram = true;
+  let lowerCase = str.toLowerCase()
+  let arr = lowerCase.split("");
+  arr.every((e,i) => {
+    if (arr.indexOf(e,i+1)<0) {
+       return true;
+    } else {
+      isogram = false;
+      return false;
+    }
+  });
+  return isogram;
 }
-
-isIsogram("sample");

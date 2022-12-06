b800b9d48746cbb641667e1805cce7f3508b0a20
Alejandro Salas <asalas.wd@outlook.com>
2022-12-05 21:09:42 -0500

    complete kata

--- string-ends-with.js
+++ string-ends-with.js
@@ -1,2 +1,6 @@
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
+function solution(str, ending){
+  // Compare sliced string with ending
+  return(str.slice(str.length-ending.length) == ending);  
+}
